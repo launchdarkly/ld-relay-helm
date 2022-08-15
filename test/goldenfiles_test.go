2031a540f27566c749459696dd86d9cfa6a86bb3
Matthew Keeler <mkeeler@launchdarkly.com>
2022-08-15 14:18:12 -0400

    Add golden ingress test

--- test/goldenfiles_test.go
+++ test/goldenfiles_test.go
@@ -29,3 +29,30 @@ func TestGoldenDefaultsTemplate(t *testing.T) {
 		})
 	}
 }
+
+func TestGoldenIngressWithBaseConfiguration(t *testing.T) {
+	t.Parallel()
+
+	chartPath, err := filepath.Abs("../")
+	require.NoError(t, err)
+
+	suite.Run(t, &golden.TemplateGoldenTest{
+		ChartPath:      chartPath,
+		Release:        "ld-relay-test",
+		Namespace:      "ld-relay-" + strings.ToLower(random.UniqueId()),
+		GoldenFileName: "ingress",
+		Templates:      []string{"templates/ingress.yaml"},
+		SetValues: map[string]string{
+			"ingress.enabled":       "true",
+			"ingress.hosts[0].host": "ld-relay.local",
+
+			"ingress.hosts[0].paths[0].path":     "/api",
+			"ingress.hosts[0].paths[0].pathType": "Prefix",
+			"ingress.hosts[0].paths[0].port":     "8030",
+
+			"ingress.hosts[0].paths[1].path":     "/prometheus",
+			"ingress.hosts[0].paths[1].pathType": "Prefix",
+			"ingress.hosts[0].paths[1].port":     "8031",
+		},
+	})
+}
