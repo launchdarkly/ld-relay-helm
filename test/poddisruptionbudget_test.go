1b13197d981f3376bc61dd73e6ce53f5722b1af5
Matthew Keeler <mkeeler@launchdarkly.com>
2025-05-05 12:48:39 -0400

    Add backwards compatibility support

--- test/poddisruptionbudget_test.go
+++ test/poddisruptionbudget_test.go
@@ -21,11 +21,42 @@ func (s *TemplateTest) TestCanEnablePDB() {
 	s.Require().Equal(0, pdb.Spec.MinAvailable.IntValue())
 }
 
+func (s *TemplateTest) TestCanEnablePDBWithDeprecatedProperty() {
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"pod.distruptionBudget.enabled": "true",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.Namespace),
+	}
+
+	output := helm.RenderTemplate(s.T(), options, s.ChartPath, s.Release, []string{"templates/poddisruptionbudget.yaml"})
+	var pdb policyv1.PodDisruptionBudget
+	helm.UnmarshalK8SYaml(s.T(), output, &pdb)
+
+	s.Require().Equal(0, pdb.Spec.MinAvailable.IntValue())
+}
+
 func (s *TemplateTest) TestCanChangePDBMaxUnavailableNumber() {
 	options := &helm.Options{
 		SetValues: map[string]string{
 			"pod.disruptionBudget.enabled":        "true",
 			"pod.disruptionBudget.maxUnavailable": "2",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.Namespace),
+	}
+
+	output := helm.RenderTemplate(s.T(), options, s.ChartPath, s.Release, []string{"templates/poddisruptionbudget.yaml"})
+	var pdb policyv1.PodDisruptionBudget
+	helm.UnmarshalK8SYaml(s.T(), output, &pdb)
+
+	s.Require().Equal(2, pdb.Spec.MaxUnavailable.IntValue())
+}
+
+func (s *TemplateTest) TestCanChangePDBMaxUnavailableNumberWithDeprecatedProperty() {
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"pod.distruptionBudget.enabled":        "true",
+			"pod.distruptionBudget.maxUnavailable": "2",
 		},
 		KubectlOptions: k8s.NewKubectlOptions("", "", s.Namespace),
 	}
@@ -53,11 +84,43 @@ func (s *TemplateTest) TestCanChangePDBMaxUnavailablePercent() {
 	s.Require().Equal("50%", pdb.Spec.MaxUnavailable.StrVal)
 }
 
+func (s *TemplateTest) TestCanChangePDBMaxUnavailablePercentWithDeprecatedProperty() {
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"pod.distruptionBudget.enabled":        "true",
+			"pod.distruptionBudget.maxUnavailable": "50%",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.Namespace),
+	}
+
+	output := helm.RenderTemplate(s.T(), options, s.ChartPath, s.Release, []string{"templates/poddisruptionbudget.yaml"})
+	var pdb policyv1.PodDisruptionBudget
+	helm.UnmarshalK8SYaml(s.T(), output, &pdb)
+
+	s.Require().Equal("50%", pdb.Spec.MaxUnavailable.StrVal)
+}
+
 func (s *TemplateTest) TestCanChangePDBMinAvailableNumber() {
 	options := &helm.Options{
 		SetValues: map[string]string{
 			"pod.disruptionBudget.enabled":      "true",
 			"pod.disruptionBudget.minAvailable": "2",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.Namespace),
+	}
+
+	output := helm.RenderTemplate(s.T(), options, s.ChartPath, s.Release, []string{"templates/poddisruptionbudget.yaml"})
+	var pdb policyv1.PodDisruptionBudget
+	helm.UnmarshalK8SYaml(s.T(), output, &pdb)
+
+	s.Require().Equal(2, pdb.Spec.MinAvailable.IntValue())
+}
+
+func (s *TemplateTest) TestCanChangePDBMinAvailableNumberWithDeprecatedProperty() {
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"pod.distruptionBudget.enabled":      "true",
+			"pod.distruptionBudget.minAvailable": "2",
 		},
 		KubectlOptions: k8s.NewKubectlOptions("", "", s.Namespace),
 	}
@@ -85,12 +148,46 @@ func (s *TemplateTest) TestCanChangePDBMinAvailablePercent() {
 	s.Require().Equal("50%", pdb.Spec.MinAvailable.StrVal)
 }
 
+func (s *TemplateTest) TestCanChangePDBMinAvailablePercentWithDeprecatedProperty() {
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"pod.distruptionBudget.enabled":      "true",
+			"pod.distruptionBudget.minAvailable": "50%",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.Namespace),
+	}
+
+	output := helm.RenderTemplate(s.T(), options, s.ChartPath, s.Release, []string{"templates/poddisruptionbudget.yaml"})
+	var pdb policyv1.PodDisruptionBudget
+	helm.UnmarshalK8SYaml(s.T(), output, &pdb)
+
+	s.Require().Equal("50%", pdb.Spec.MinAvailable.StrVal)
+}
+
 func (s *TemplateTest) TestBothPDBValues() {
 	options := &helm.Options{
 		SetValues: map[string]string{
 			"pod.disruptionBudget.enabled":        "true",
 			"pod.disruptionBudget.minAvailable":   "50%",
 			"pod.disruptionBudget.maxUnavailable": "2",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", s.Namespace),
+	}
+
+	output := helm.RenderTemplate(s.T(), options, s.ChartPath, s.Release, []string{"templates/poddisruptionbudget.yaml"})
+	var pdb policyv1.PodDisruptionBudget
+	helm.UnmarshalK8SYaml(s.T(), output, &pdb)
+
+	s.Require().Equal(2, pdb.Spec.MaxUnavailable.IntValue())
+	s.Require().Nil(pdb.Spec.MinAvailable)
+}
+
+func (s *TemplateTest) TestBothPDBValuesWithDeprecatedProperty() {
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"pod.distruptionBudget.enabled":        "true",
+			"pod.distruptionBudget.minAvailable":   "50%",
+			"pod.distruptionBudget.maxUnavailable": "2",
 		},
 		KubectlOptions: k8s.NewKubectlOptions("", "", s.Namespace),
 	}
